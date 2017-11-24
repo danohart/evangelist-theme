69516129692ececac185a0b653433de168661674
Daniel Hart <me@danielhart.co>
2017-11-23 23:16:58 -0600

    Sidebar cleanup

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,31 @@
 <div id="left-col" class="borderbox">
- <img src="<?php bloginfo('template_url'); ?>/images/family.jpg"/>
- <h3 align="center">Johna, Steve, &amp; Andrew Hart</h3>
+	<img src="<?php bloginfo('template_url'); ?>/images/family.jpg"/>
+	<h3 align="center">Johna, Steve, &amp; Andrew Hart</h3>
+	<div id="nav">
+		<ul>
+			<?php wp_nav_menu( array('menu' => 'Main Nav', 'theme_location' => 'main-menu', 'items_wrap' => '%3$s', 'container' => 'false'  )); ?>
+		</ul>
+	</div>
  
- <div id="nav">
-  <ul>
-      <?php wp_nav_menu( array('menu' => 'Main Nav', 'theme_location' => 'main-menu', 'items_wrap' => '%3$s', 'container' => 'false'  )); ?>
-  </ul>
- </div>
- 
- <div class="contact-info"><i class="fa fa-mobile"></i><a href="tel:8122012286">812.201.2286</a></div>
- <div class="contact-info"><i class="fa fa-envelope-square"></i><a href="mailto:broshart@gmail.com">broshart@gmail.com</a></div>
- <div class="contact-info"><i class="fa fa-twitter-square"></i><a href="http://twitter.com/preacherhart">@PreacherHart</a></div><br/>
- <div class="contact-info">Support Address: <br/>c/o First Baptist Church<br/>220 Illini Dr.<br/>Clinton, IL 61727</div>
- <div class="clearfix"></div>
+	<div class="contact-info">
+	 	<i class="fa fa-mobile"></i><a href="tel:8122012286">812.201.2286</a>
+	</div>
 
-	 <p>Steve Hart is available for Revival meetings, Camp Meetings, Bible Conferences, Youth Camps, Special Sundays, etc...</p>
+	<div class="contact-info">
+		<i class="fa fa-envelope-square"></i><a href="mailto:broshart@gmail.com">broshart@gmail.com</a>
+	</div>
 
- 
+	<div class="contact-info">
+		<i class="fa fa-twitter-square"></i><a href="http://twitter.com/preacherhart">@PreacherHart</a>
+	</div>
+
+	<br/>
+
+	<div class="contact-info">
+		Support Address: <br/>c/o First Baptist Church<br/>220 Illini Dr.<br/>Clinton, IL 61727
+	</div>
+
+	<div class="clearfix"></div>
+	
+	<p>Steve Hart is available for Revival meetings, Camp Meetings, Bible Conferences, Youth Camps, Special Sundays, etc...</p>
 </div>
